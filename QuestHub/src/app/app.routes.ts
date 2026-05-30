682535af34035cb936f57c62b37532fbddbfd351
Stefano Buglione <stefanobc123456@gmail.com>
2026-05-29 19:52:27 -0500

    basic project setup

---
+++ QuestHub/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginPage } from './login-page/login-page';
+
+export const routes: Routes = [
+    {path: "", component: LoginPage, pathMatch:"full"}
+];
